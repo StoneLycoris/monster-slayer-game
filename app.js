40e3d003154a27b191ba53c3dd3ad0d60b4164fa
Elena Zhilinskaia <zhilinskaia.elena@gmail.com>
2022-05-21 15:50:15 +0300

    Added surrender and start new game functionality

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ const app = Vue.createApp({
 	},
 	computed: {
 		monsterBarStyles() {
+			if (this.monsterHealth < 0) {
+				this.monsterHealth = 0;
+				return { width: "0%" };
+			}
 			return { width: this.monsterHealth + "%" };
 		},
 		playerBarStyles() {
+			if (this.playerHealth < 0) {
+				this.playerHealth = 0;
+				return { width: "0%" };
+			}
 			return { width: this.playerHealth + "%" };
 		},
 		mayUseSpecialAttack() {
@@ -46,6 +54,12 @@ const app = Vue.createApp({
 		},
 	},
 	methods: {
+		startGame() {
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+			this.winner = null;
+			this.currentRound = 0;
+		},
 		attackMonster() {
 			this.currentRound++;
 
@@ -77,6 +91,9 @@ const app = Vue.createApp({
 			}
 
 			this.attackPlayer();
+		},
+		surrender() {
+			this.winner = "monster";
 		},
 	},
 });
